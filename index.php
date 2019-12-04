f15ad9d64af88ab884b47bdab768d4d9c22539ed
kiema kibe <kiemaken@gmail.com>
2019-12-04 17:35:05 +0300

    my first php assignment

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+echo strlen("+254710777347");
+
+echo "<br><br>";
+$contact= "257710777347";
+switch($contact){
+    case "254710777347":
+    echo "Kenya";
+    break;
+
+    case "256710777347":
+    echo "Uganda";
+    break;
+
+    case "255710777347":
+    echo "Tanzania";
+    break;
+
+    case "257710777347":
+    echo "Rwanda";
+    break;
+
+default:
+echo "input a valid contact";
+}
+?>
